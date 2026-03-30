02a4158bc8d2cc3f5713dafd963cca452300605c
01nijat <mr.nijat.asgarov@gmail.com>
2026-03-31 01:01:15 +0400

    Logic

--- Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2ddf38fa8a29ba993509feb62d11bc19dbf026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8857b59e194fe5777cac2acc725489e854e69cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
