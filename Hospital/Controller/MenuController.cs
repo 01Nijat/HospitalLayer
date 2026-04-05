edc64c85a30af063861ebbfc95bf58a4296d0a4a
01nijat <mr.nijat.asgarov@gmail.com>
2026-04-05 12:16:01 +0400

    controller

---
+++ Hospital/Controller/MenuController.cs
@@ -0,0 +1,48 @@
+using System;
+using Utilies.Helper;
+
+namespace Hospital.Controller;
+
+public class MenuController
+{
+  DepartamentController departamentController=new DepartamentController();
+   public void DepartamentMenu()
+    {
+        while (true)
+        {
+            Helper.TextColor(ConsoleColor.White, "Departament ucun bir secim edin");
+            Helper.TextColor(ConsoleColor.White, "please correct opnion: 1-Create, 2-DepartamenUpdate, 3-DeleteDepartament, 4-GetDepartamentwithName, 5-GetDepartamentWithId, 6-GetAllDe,7-GetAllEmplooye");
+            string secim=Console.ReadLine();
+            int menu;
+            bool isTrue=int.TryParse(secim, out menu);
+            if (isTrue)
+            {
+     switch (menu)
+   {
+          case (int)Menu.Create:
+           departamentController.Create();
+           break;
+        case  (int)Menu.Update:
+        departamentController.Update();        
+        break;
+        case (int)Menu.Delete:
+        departamentController.Delete();
+        break;
+        case (int)Menu.GetByName:
+        departamentController.GetByName();
+        break;
+        case (int)Menu.GetById:
+        departamentController.GetById();
+        break;
+        case (int)Menu.GetAll:
+        departamentController.GetAll();
+        break;
+        case (int)Menu.GetAllEmployee:
+        departamentController.GetAllEmplooye();       
+        break;
+    default: Helper.TextColor(ConsoleColor.Red, "Yanlish secim"); break;
+                }
+            }
+        }
+    }
+}
--- Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe382f0b3cc55f47570ba6c60b767526825f958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1493d9545db4f2b01215bd588335b3fe50e6d5f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
