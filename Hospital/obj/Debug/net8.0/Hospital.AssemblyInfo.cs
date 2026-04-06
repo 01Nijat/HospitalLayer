fd704d5f478a24ec11db6f22192cc285db6e4743
01nijat <mr.nijat.asgarov@gmail.com>
2026-04-06 22:16:27 +0400

    controller

--- Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
+++ Hospital/obj/Debug/net8.0/Hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4710197e97184ea3569a85935c655b1b8ca56d76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f574d20b7341317d1c05b5eebd3acc934460f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
